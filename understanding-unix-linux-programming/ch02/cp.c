a32dd053e0bc5256af5cfbaeb3ad6d07549fc60a
jocs <ransixi@gmail.com>
2021-11-13 23:25:49 +0800

    uulp: optimization of cp command line, cp a folder only with files

--- understanding-unix-linux-programming/ch02/cp.c
+++ understanding-unix-linux-programming/ch02/cp.c
@@ -4,58 +4,86 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <dirent.h>
 
 #define BUFFER_SIZE 20
 #define FILENAME_LEN 256
 #define COPYMODE 0644
 
 void err_handler(char* filename);
+void do_copy(char* source, char* dist);
 
 int main(int argc, char** argv) {
   if (argc != 3) {
     fprintf(stderr, "cp user error\n");
     exit(EXIT_FAILURE);
   }
+  char source_filename[FILENAME_LEN];
+  struct stat st;
+
+  stat(argv[1], &st);
+
+  if (S_ISDIR(st.st_mode)) {
+    DIR* dir;
+    struct dirent* dr;
+    if ((dir = opendir(argv[1])) == NULL) {
+      err_handler(argv[1]);
+    }
+    while ((dr = readdir(dir)) != NULL) {
+      // 没有处理 folder 包含其它文件夹的 case，可以过滤或者递归复制。
+      if (strcmp(dr->d_name, ".") == 0 || strcmp(dr->d_name, "..") == 0) {
+        continue;
+      }
+      strcpy(source_filename, argv[1]);
+      strcat(source_filename, "/");
+      strcat(source_filename, dr->d_name);
+      do_copy(source_filename, argv[2]);
+    }
+    closedir(dir);
+  } else {
+
+    do_copy(source_filename, argv[2]);
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
+void do_copy(char* source, char* dist) {
   int in_fd;
   int out_fd;
   int count;
   int buf[BUFFER_SIZE];
-  char dist_filename[FILENAME_LEN];
+  int dist_filename[FILENAME_LEN];
   struct stat st;
 
-  if ((in_fd = open(argv[1], O_RDONLY)) == -1) {
-    err_handler(argv[1]);
+  if ((in_fd = open(source, O_RDONLY)) == -1) {
+    err_handler(source);
   }
 
-  if (stat(argv[2], &st) == -1) {
-    // err_handler(argv[2]);
-  }
-
-  strcpy(dist_filename, argv[2]);
+  stat(dist, &st);
+  strcpy(dist_filename, dist);
   if (S_ISDIR(st.st_mode)) {
     strcat(dist_filename, "/");
-    strcat(dist_filename, strstr(argv[1], "/") ? strstr(argv[1], "/") + 1 : argv[1]);
+    strcat(dist_filename, strstr(source, "/") ? strstr(source, "/") + 1 : source);
   }
 
   if ((out_fd = creat(dist_filename, COPYMODE)) == -1) {
-    err_handler(argv[2]);
+    err_handler(dist_filename);
   }
 
   while ((count = read(in_fd, buf, BUFFER_SIZE)) > 0) {
     if ((write(out_fd, buf, count)) != count) {
-      err_handler(argv[2]);
+      err_handler(dist_filename);
     }
   }
 
   if (count == -1) {
-    err_handler(argv[1]);
+    err_handler(source);
   }
 
   if (close(in_fd) == -1 || close(out_fd) == -1) {
-    err_handler(argv[1]);
+    err_handler(source);
   }
-
-  exit(EXIT_SUCCESS);
 }
 
 void err_handler(char* filename) {
