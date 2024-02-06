8ac6b5fff842c059c3c733e2be4a1c88e8c66f37
jocs <luoran1988@126.com>
2024-02-06 23:20:29 +0800

    hecto: handle delete backspace and enter key

--- hecto/src/row.rs
+++ hecto/src/row.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
+#[derive(Default)]
 pub struct Row {
     pub string: String,
     len: usize,
@@ -39,6 +40,55 @@ impl Row {
         }
 
         result
+    }
+
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len {
+            self.string.push(c);
+        } else {
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let remain: String = self.string[..].graphemes(true).skip(at).collect();
+
+            result.push(c);
+
+            result.push_str(&remain);
+
+            self.string = result;
+        }
+
+        self.update_len();
+    }
+
+    pub fn delete(&mut self, at: usize) {
+        if at >= self.len {
+            return;
+        }
+
+        let mut result: String = self.string[..].graphemes(true).take(at).collect();
+        let remain: String = self.string[..].graphemes(true).skip(at + 1).collect();
+
+        if remain.len() > 0 {
+            result.push_str(&remain);
+        }
+
+        self.string = result;
+        self.update_len();
+    }
+
+    pub fn append(&mut self, other: &Self) {
+        self.string.push_str(&other.string);
+
+        self.update_len();
+    }
+
+    pub fn split(&mut self, at: usize) -> Self {
+        let result: String = self.string[..].graphemes(true).take(at).collect();
+        let remain: String = self.string[..].graphemes(true).skip(at + 1).collect();
+
+        self.string = result;
+        self.update_len();
+
+        Self::from(remain.as_str())
     }
 
     pub fn len(&self) -> usize {
