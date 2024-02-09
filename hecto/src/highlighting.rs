d6b27395b41df1ff10ac2344d632fd95628d3889
jocs <luoran1988@126.com>
2024-02-10 00:51:22 +0800

    hecto: basic highlight

---
+++ hecto/src/highlighting.rs
@@ -0,0 +1,16 @@
+use termion::color;
+
+#[derive(PartialEq)]
+pub enum Type {
+  None,
+  Number,
+}
+
+impl Type {
+    pub fn to_color(&self) -> impl color::Color {
+      match self {
+        Type::Number => color::Rgb(220, 163, 163),
+        Type::None => color::Rgb(255, 255, 255),
+      }
+    }
+}
