ba6c2e751bb6290b4333820eea39e638aa0e0157
jocs <luoran1988@126.com>
2024-02-10 09:14:04 +0800

    hetoc: highlight search results

--- hecto/src/highlighting.rs
+++ hecto/src/highlighting.rs
@@ -4,12 +4,14 @@ use termion::color;
 pub enum Type {
   None,
   Number,
+  Match,
 }
 
 impl Type {
     pub fn to_color(&self) -> impl color::Color {
       match self {
         Type::Number => color::Rgb(220, 163, 163),
+        Type::Match => color::Rgb(38, 139, 210),
         Type::None => color::Rgb(255, 255, 255),
       }
     }
